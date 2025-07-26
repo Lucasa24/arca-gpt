76b514922e1fddf54bebb284f177a1385367b793
Lucasa24 <lucasalvarezempresa@gmail.com>
2025-07-26 00:54:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ async function sendMessage() {
   loader.style.display = "block";
   loadingBar.style.width = "0%";
 
+  // Animação simulada da barra de carregamento
+  let width = 0;
+  const interval = setInterval(() => {
+    if (width >= 90) {
+      clearInterval(interval);
+    } else {
+      width += 1;
+      loadingBar.style.width = width + "%";
+    }
+  }, 25); // velocidade da animação
+
   try {
     const res = await fetch("/api/arca", {
       method: "POST",
@@ -16,8 +27,21 @@ async function sendMessage() {
     });
 
     const data = await res.json();
+
+   // Finaliza a barra
+    clearInterval(interval);
+    loadingBar.style.width = "100%";
+
+// Aguarda um instante e esconde a barra
+    setTimeout(() => {
+      loader.style.display = "none";
+      responseDiv.innerHTML = data.reply;
+    }, 300);
+
     responseDiv.innerHTML = data.reply;
   } catch (error) {
+    clearInterval(interval);
+    loader.style.display = "none";
     responseDiv.innerHTML = "Erro na invocação: " + error.message;
   }
 }
